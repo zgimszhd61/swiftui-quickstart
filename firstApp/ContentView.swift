07707242dbbf2c33e9518ee2f6e94eb51a154d01
xiaoming <lininruc@gmail.com>
2024-03-09 17:45:42 +0800

    add mvs workflow

---
+++ firstApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firstApp
+//
+//  Created by mac on 2024/3/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firstApp/firstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppApp.swift
+//  firstApp
+//
+//  Created by mac on 2024/3/9.
+//
+
+import SwiftUI
+
+@main
+struct firstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
